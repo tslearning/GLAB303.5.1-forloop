b8e1ce61e93e788aed2140953b36a31aa3c35694
Touyer Lee <touyer.lees@gmail.com>
2024-11-24 22:37:11 -0600

    Initial commit

---
+++ src/LoopDemo.java
@@ -0,0 +1,10 @@
+public class LoopDemoOne {
+    public static void main(String args[]){
+        //Program to print a text 5 times
+        int n = 5;
+        //for loop
+        for(int i = 1; i <= n; i++){
+            System.out.println("Java is fun");
+        }
+    }
+}
