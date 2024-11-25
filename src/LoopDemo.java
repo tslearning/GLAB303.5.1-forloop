8cce55f50fd8fc6013474f863ea109d928147518
Touyer Lee <touyer.lees@gmail.com>
2024-11-24 22:37:53 -0600

    GLAB 303.5.1 - for loop completed

--- src/LoopDemo.java
+++ src/LoopDemo.java
@@ -1,10 +1,51 @@
-public class LoopDemoOne {
-    public static void main(String args[]){
-        //Program to print a text 5 times
-        int n = 5;
+import java.util.Scanner;
+
+public class LoopDemo {
+
+    public void loopDemoOne() {
+        //Example 1: Program to print a text 5 times
+        int n = 5; // Assigning 5 to n
         //for loop
-        for(int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) { // for loop equation to run 5 time
             System.out.println("Java is fun");
         }
     }
+
+    //Example 2: Display Sum of n Natural Numbers
+    public void loopDemoTwo() {
+        int sum = 0;
+        int n = 1000;
+        //for loop
+        for(int i = 1; i <= n; ++i) {
+            //body inside for loop
+            sum += i; //sum = sum + i
+        }
+        System.out.println("Sum is: " + sum);
+    }
+
+    public void palindromeExample() {
+        String original = "";
+        String reverse = ""; //Objects of String class
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a string/number to check if it is a palindrome");
+        original = in.nextLine(); //racecar
+        int length = original.length();
+        for (int i = length - 1; i >= 0; i--)
+            reverse = reverse + original.charAt(i);
+        if (original.equals(reverse)) {
+            System.out.println("Entered string/number is a palindrome.");
+        } else {
+            System.out.println("Entered string/number isn't a palindrome.");
+        }
+    }
+
+    public void pyramidExample(){
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+    LoopDemo loopExample = new LoopDemo();
+//    loopExample.loopDemoOne();
+//    loopExample.loopDemoTwo();
+//    loopExample.palindromeExample();
+    loopExample.pyramidExample();
+    }
 }
